fe327fecd4849bc60976e6309ba8eb867a2a6e16
kdywan_254049 <idywan_254049@e-science.pl>
2023-04-05 22:55:35 +0200

    Added fetching data from backend on main page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -155,10 +155,10 @@ const Home = () => {
     return (
         <CategoriesContent>
             <Grid item container direction="column" rowGap={8} marginTop={2}>
-                <EbooksSlider title="Nowości" ebooks={ebooks}/>
-                <EbooksSlider title="Promocje" ebooks={ebooks}/>
-                <EbooksSlider title="Bestsellery" ebooks={ebooks}/>
-                <EbooksSlider title="Najlepsze książki 2022 roku" ebooks={ebooks}/>
+                <EbooksSlider title="Nowości" sort={EbookSortOptions[EbookSortOptions.DescByDate]}/>
+                <EbooksSlider title="Promocje" sort={EbookSortOptions[EbookSortOptions.DescByDate]}/>
+                <EbooksSlider title="Bestsellery" sort={EbookSortOptions[EbookSortOptions.DescByDate]}/>
+                <EbooksSlider title="Najlepsze książki 2022 roku" sort={EbookSortOptions[EbookSortOptions.DescByDate]}/>
             </Grid>
         </CategoriesContent>
     )
