41caf9a9667cf1f8acee16633134051577d3d0d2
DamEmi64 <84103878+DamEmi64@users.noreply.github.com>
2023-04-25 16:26:44 +0200

    ebook user

--- Ksiegarnia/Tests/Controllers/UserController/DetailsTest.cs
+++ Ksiegarnia/Tests/Controllers/UserController/DetailsTest.cs
@@ -3,6 +3,7 @@ using Domain.Entitites;
 using Domain.Repositories;
 using Infrastructure.Exceptions;
 using Infrastructure.Services.Interfaces;
+using Microsoft.Extensions.Hosting;
 using Moq;
 
 namespace Tests.Controllers.UserController
@@ -10,6 +11,7 @@ namespace Tests.Controllers.UserController
     public class DetailsTest
     {
         private string userId = "TEST";
+        private IHostEnvironment env = new Mock<IHostEnvironment>().Object;
 
         [Fact]
         public async Task Failed_UserNotFound_empty()
@@ -18,7 +20,7 @@ namespace Tests.Controllers.UserController
             userRepo.Setup(x => x.Get(userId)).ReturnsAsync(new User());
             var authService = new Mock<ISmtpService>();
 
-            var controller = new UsersController(userRepo.Object, authService.Object);
+            var controller = new UsersController(userRepo.Object, authService.Object, env);
 
             var result = await controller.Details(userId);
             await Assert.ThrowsAsync<UserNotFoundException>(async () => await controller.Details(string.Empty));
@@ -31,7 +33,7 @@ namespace Tests.Controllers.UserController
             userRepo.Setup(x => x.Get(userId)).ReturnsAsync(new User());
             var authService = new Mock<ISmtpService>();
 
-            var controller = new UsersController(userRepo.Object, authService.Object);
+            var controller = new UsersController(userRepo.Object, authService.Object, env);
 
             var result = await controller.Details(userId);
             Assert.ThrowsAsync<UserNotFoundException>(async () => await controller.Details("TEST2"));
@@ -44,7 +46,7 @@ namespace Tests.Controllers.UserController
             userRepo.Setup(x => x.Get(userId)).ReturnsAsync(new User());
             var authService = new Mock<ISmtpService>();
 
-            var controller = new UsersController(userRepo.Object, authService.Object);
+            var controller = new UsersController(userRepo.Object, authService.Object, env);
 
             var result = await controller.Details(userId);
             Assert.NotNull(result);
--- Ksiegarnia/Tests/Controllers/UserController/RegisterTest.cs
+++ Ksiegarnia/Tests/Controllers/UserController/RegisterTest.cs
@@ -3,12 +3,15 @@ using Domain.DTOs;
 using Domain.Repositories;
 using Infrastructure.Exceptions;
 using Infrastructure.Services.Interfaces;
+using Microsoft.Extensions.Hosting;
 using Moq;
 
 namespace Tests.Controllers.UserController
 {
     public class RegisterTest
     {
+
+        private IHostEnvironment env = new Mock<IHostEnvironment>().Object;
         [Fact]
         public async Task Failed_FailedRegister()
         {
@@ -24,7 +27,7 @@ namespace Tests.Controllers.UserController
             var userRepo = new Mock<IUserRepository>();
             var authService = new Mock<ISmtpService>();
 
-            var controller = new UsersController(userRepo.Object, authService.Object);
+            var controller = new UsersController(userRepo.Object, authService.Object, env);
             await Assert.ThrowsAsync<RegisterFailedException>(async () => await controller.Register(data));
         }
     }
